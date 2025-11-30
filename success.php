3aeb49d3a07cfb9618055ee825a7c49dbb774b51
sorse-maaan <sorse1000@gmail.com>
2025-12-01 00:52:17 +0200

    Create success.php

---
+++ success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+<meta charset="UTF-8">
+<title>Оплата успішна</title>
+</head>
+<body>
+<h2>Дякуємо! Оплату отримано.</h2>
+<p>Ваше замовлення обробляється.</p>
+</body>
+</html>
